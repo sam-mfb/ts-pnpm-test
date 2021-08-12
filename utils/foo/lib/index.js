c17cf50a2e0891b09381e5f9abc8120dede892c0
sam <sam@mfbtech.com>
2021-08-12 05:21:09 -0400

    add code

---
+++ utils/foo/lib/index.js
@@ -0,0 +1,4 @@
+export function myFunction() {
+    var foo = "hello, world";
+    return foo;
+}
---
+++ utils/foo/src/index.ts
@@ -0,0 +1,6 @@
+import "src/../node_modules/ts-jest"
+
+export function myFunction() {
+  const foo = "hello, world"
+  return foo
+}
