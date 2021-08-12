5cd3deb6f97f346d9600324b37052ae1b0b70a7a
sam <sam@mfbtech.com>
2021-08-12 05:33:23 -0400

    setup test

--- utils/foo/src/index.ts
+++ utils/foo/src/index.ts
@@ -1,6 +1,11 @@
-import "src/../node_modules/ts-jest"
+// uncomment the line below for auto-import to work
+//import "../node_modules/ts-jest"
 
 export function myFunction() {
   const foo = "hello, world"
   return foo
 }
+
+// no auto-import or intellisense suggestiosn unless
+// line 2 is uncommented
+useEffect
